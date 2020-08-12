a9e8ac64d5ded42297fae116e683dfdab0d1a04f
Jairo Rojas <jairo142014@hotmail.com>
2020-08-12 14:01:29 -0500

    componentes de Chat y enviar mensaje creado creado, registro de msj en firebase hecho

---
+++ src/Components/Chat/Messages.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    
+  },
+  paper: {
+    paddingBottom: 20,
+    paddingTop: 20,
+    marginTop:5,
+    border: "1px solid #ccc",
+  },
+  message: {
+    border: "0px solid #ccc",
+    borderRadius: 8,
+    padding: "5px 20px 5px",
+  },
+  box: {
+    height: "100%",
+    paddingLeft: 20,
+  },
+}));
+
+const Message = () => {
+  const classes = useStyles();
+  const [message, setMessage] = useState("");
+
+  //Con este arreglo de funciones se envia los mensajes y se actualiza el state
+  const haldleSubmit = (e) => {
+    e.preventDefault();
+
+    const { currentUser } = firebase.auth();
+    if (!currentUser) return;
+
+    const NewMessage = {
+      user: currentUser.uid,
+      message,
+      date: firebase.database.ServerValue.TIMESTAMP,
+    };
+    firebase
+      .database()
+      .ref("/chat")
+      .push(NewMessage)
+      .then((response) => {
+        setMessage("");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error.message);
+      });
+  };
+
+  return (
+    <Paper square className={classes.paper}>
+      <form onSubmit={haldleSubmit}>
+        <Grid container spacing={0} direction="row" className={classes.message}>
+          <Grid item xs={10}>
+            <TextField
+              variant="outlined"
+              name="message"
+              required
+              fullWidth
+              id="message"
+              label="Escribe un mensaje..."
+              autoFocus
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <Box display="flex" alignItems="center" className={classes.box}>
+              <Button
+                variant="contained"
+                color="primary"
+                endIcon={
+                  <Icon alignItems="center">
+                    send
+                  </Icon>
+                }
+                fullWidth
+                disabled={!message.length}
+              >Enviar</Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </form>
+    </Paper>
+  );
+};
+
+export default Message;
--- src/Components/Chat/index.jsx
+++ src/Components/Chat/index.jsx
@@ -1,24 +1,99 @@
+import React, { useState, useEffect, useRef } from "react";
+import { withRouter } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from '@material-ui/core/Avatar';
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
 
-import React, { useState, useEffect, useRef } from 'react';
-import { withRouter } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Paper from '@material-ui/core/Paper';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import firebase from 'firebase/app';
-import 'firebase/database';
-import 'firebase/auth';
+import Message from './Messages'
 
+const useStyles = makeStyles((theme) => ({
+  text: {
+    padding: theme.spacing(2, 2, 0),
+  },
+  paper: {
+    paddingBottom: 50,
+    height: '70vh',
+    marginTop: 20,
+  },
+  list: {
+    marginBottom: theme.spacing(3),
+    maxHeight: '95%',
+    overflow: 'auto'
+  },
+}));
 
+const Chat = () => {
+  const classes = useStyles();
+  const [messages, setMessages] = useState([]);
 
-const Chat = () =>{
-    return(
-        <h1>Bienvenido</h1>
-    )
-}
+  const chatDomRef = useRef();
+
+  const addMessageList = message =>{
+      messages.push(message);
+
+      setMessages([...messages]);
+
+      if (chatDomRef.current) {
+        chatDomRef.current.scrollTop = chatDomRef.current.scrollHeight;
+      }
+  }
+
+  useEffect(() => {
+    const chatRef = firebase.database().ref('/chat');
+
+    chatRef.on(
+      'child_added',
+      snapshot => {
+        //New Message
+        const messageItem = snapshot.val();
+        //data user
+        firebase
+          .database()
+          .ref(`/users/${messageItem.user}`)
+          .once("value")
+          .then((userResponse) => {
+            messageItem.user = userResponse.val();
+            addMessageList(messageItem);
+          });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  },[]);
+
+  return (
+    <Container>
+      <Paper square className={classes.paper}>
+        <Typography className={classes.text} variant="h5" gutterBottom style={{ backgroundColor: "#00bcd4",color: "white" }}>
+          Chats
+        </Typography>
+        <List className={classes.list} ref={chatDomRef}>
+          {messages.map(({ date, user, message }) => (
+            <ListItem button key={date}>
+              <ListItemAvatar>
+                <Avatar alt={user.name} src={user.avatar} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={user ? user.name : "annonymous"}
+                secondary={message}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+      <Message/>
+    </Container>
+  );
+};
 
 export default withRouter(Chat);
